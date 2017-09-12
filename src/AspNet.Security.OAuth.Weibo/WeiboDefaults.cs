87b6242b9fdfe24a3f6685e6f0c19ccce3b93689
wujuntao <wujuntaocn@outlook.com>
2017-09-12 18:10:32 +0800

    Migrate OAuth.Weibo to AspNetCore 2.0

--- src/AspNet.Security.OAuth.Weibo/WeiboDefaults.cs
+++ src/AspNet.Security.OAuth.Weibo/WeiboDefaults.cs
@@ -4,6 +4,7 @@
  * for more information concerning the license and the contributors participating to this project.
  */
 
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 
 namespace AspNet.Security.OAuth.Weibo
@@ -11,41 +12,20 @@ namespace AspNet.Security.OAuth.Weibo
     /// <summary>
     /// Default values for Weibo authentication.
     /// </summary>
-    public static class WeiboAuthenticationDefaults
+    public static class WeiboDefaults
     {
-        /// <summary>
-        /// Default value for <see cref="AuthenticationOptions.AuthenticationScheme"/>.
-        /// </summary>
         public const string AuthenticationScheme = "Weibo";
 
-        /// <summary>
-        /// Default value for <see cref="RemoteAuthenticationOptions.DisplayName"/>.
-        /// </summary>
         public const string DisplayName = "Weibo";
 
-        /// <summary>
-        /// Default value for <see cref="RemoteAuthenticationOptions.CallbackPath"/>.
-        /// </summary>
         public const string CallbackPath = "/signin-weibo";
 
-        /// <summary>
-        /// Default value for <see cref="AuthenticationOptions.ClaimsIssuer"/>.
-        /// </summary>
         public const string Issuer = "Weibo";
 
-        /// <summary>
-        /// Default value for <see cref="OAuthOptions.AuthorizationEndpoint"/>.
-        /// </summary>
         public const string AuthorizationEndpoint = "https://api.weibo.com/oauth2/authorize";
 
-        /// <summary>
-        /// Default value for <see cref="OAuthOptions.TokenEndpoint"/>.
-        /// </summary>
         public const string TokenEndpoint = "https://api.weibo.com/oauth2/access_token";
 
-        /// <summary>
-        /// Default value for <see cref="OAuthOptions.UserInformationEndpoint"/>.
-        /// </summary>
         public const string UserInformationEndpoint = "https://api.weibo.com/2/users/show.json";
     }
 }
---
+++ src/AspNet.Security.OAuth.Weibo/WeiboExtensions.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System;
+using Microsoft.AspNetCore.Authentication;
+using AspNet.Security.OAuth.Weibo;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class WeiboExtensions
+    {
+        public static AuthenticationBuilder AddMicrosoftAccount(this AuthenticationBuilder builder)
+            => builder.AddMicrosoftAccount(WeiboDefaults.AuthenticationScheme, _ => { });
+
+        public static AuthenticationBuilder AddMicrosoftAccount(this AuthenticationBuilder builder, Action<WeiboOptions> configureOptions)
+            => builder.AddMicrosoftAccount(WeiboDefaults.AuthenticationScheme, configureOptions);
+
+        public static AuthenticationBuilder AddMicrosoftAccount(this AuthenticationBuilder builder, string authenticationScheme, Action<WeiboOptions> configureOptions)
+            => builder.AddMicrosoftAccount(authenticationScheme, WeiboDefaults.DisplayName, configureOptions);
+
+        public static AuthenticationBuilder AddMicrosoftAccount(this AuthenticationBuilder builder, string authenticationScheme, string displayName, Action<WeiboOptions> configureOptions)
+            => builder.AddOAuth<WeiboOptions, WeiboHandler>(authenticationScheme, displayName, configureOptions);
+
+    }
+}
